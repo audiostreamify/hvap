93303f3de9f6dc9519425a905c780f92985fcce0
Donald Pakkies <donaldpakkies@gmail.com>
2019-01-16 18:15:25 +0200

    Feat: return null if image is not set

--- src/Mocks/Cover.php
+++ src/Mocks/Cover.php
@@ -9,9 +9,9 @@ trait Cover
   /**
    * Get album art as base64 encoded data
    *
-   * @return string
+   * @return string|null
    */
-  public function cover() : string
+  public function cover()
   {
     /**
      * Instantiate the getID3 class
@@ -23,6 +23,16 @@ trait Cover
      * Then return the image as a base64 encoded string
      */
     $info  = $getID3->analyze($this->file);
+
+    /**
+     * If image cannot be found, return null
+     */
+    if (
+      !isset($info['comments']['picture'][0]['data']) ||
+      $info['comments']['picture'][0]['data'] == null
+    )
+      return null;
+
     $image = $getID3->GetFileFormat($info['comments']['picture'][0]['data']);
 
     return 'data:' . $image['mime_type'] . ';base64,' . base64_encode($info['comments']['picture'][0]['data']);
